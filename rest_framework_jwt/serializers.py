a3b07cad095fc346f0f83987c7243b129fcf8655
Samir Ahmed <blaklites@gmail.com>
2016-06-12 09:27:21 +0600

    Directly access user model

--- rest_framework_jwt/serializers.py
+++ rest_framework_jwt/serializers.py
@@ -3,7 +3,8 @@ import jwt
 from calendar import timegm
 from datetime import datetime, timedelta
 
-from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 from rest_framework import serializers
 from .compat import Serializer
@@ -12,7 +13,6 @@ from rest_framework_jwt.settings import api_settings
 from rest_framework_jwt.compat import get_username_field, PasswordField
 
 
-User = get_user_model()
 jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
 jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 jwt_decode_handler = api_settings.JWT_DECODE_HANDLER
